3f1d202e711239996255d6a9c1e3627d1b7fb0a4
hilarynishafounder-afk <hilarynishafounder@gmail.com>
2026-03-20 12:21:06 +0530

    frontend problem

--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.VITE_API_URL || "https://sproutsorgs-backend-k7fx.onrender.com/api";
+export const BASE_URL = import.meta.env.VITE_API_URL || "https://sproutsorgs-backend-8xrf.onrender.com/api";
